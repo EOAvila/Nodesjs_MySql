8c0525b157fa9f31d7449d94f45bbc2393f34fcc
Edwin Avila <edwin.avila71@hotmail.com>
2026-06-02 16:22:57 -0600

    Actualización de la API - Ver. 1.0.0 - 02.06.2026 / 16:22 p.m.

--- src/config/bitrix.js
+++ src/config/bitrix.js
@@ -2,5 +2,5 @@ import dotenv from 'dotenv'
 
 dotenv.config( )
 
-export const BITRIX_WEBHOOK_IN  = 'https://calipso.bitrix24.com/rest/32/j67s3tlp1j8b1jjv/'
-export const BITRIX_WEBHOOK_OUT = 'https://nodesjs-mysql.onrender.com/api/webhook/bitrix/'
+export const BITRIX_WEBHOOK_IN  = 'https://calipso.bitrix24.com/rest/32/6bw3zqsscf14e336/'
+export const BITRIX_WEBHOOK_OUT = 'https://nodesjs-mysql.onrender.com/webhook/bitrix/'
