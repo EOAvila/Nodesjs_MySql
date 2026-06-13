b4a3727a0532e492d9a9032cf8339e3431ea879a
Edwin Avila <edwin.avila71@hotmail.com>
2026-06-13 09:51:11 -0600

    Actualización de la API - Ver. 1.0.3 - 13.06.2026 / 09:50 a.m.

---
+++ src/services/inventario.service.js
@@ -0,0 +1,22 @@
+export const listarInventario = async () => {
+
+    const sql = `
+        SELECT
+            p.id,
+            p.bitrix_id,
+            p.nombre,
+            p.precio,
+            i.stock_actual,
+            i.stock_minimo,
+            i.stock_maximo
+        FROM pro_productos p
+        INNER JOIN pro_inventario i
+            ON p.id = i.producto_id
+        ORDER BY p.nombre
+    `;
+
+    const [rows] =
+        await pool.query(sql);
+
+    return rows;
+};
