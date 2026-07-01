fa28dfb87289a5a0affcc7bad6c39fe84da980d3
Edwin Avila <edwin.avila71@hotmail.com>
2026-06-30 21:59:00 -0600

    Actualización de la API - Ver. 1.0.4 - 30.06.2026 / 21:58 p.m.

--- src/controllers/bitrix.controller.js
+++ src/controllers/bitrix.controller.js
@@ -286,7 +286,7 @@ const procesarWebhookBitrix = async (body) => {
         apellido: contacto.LAST_NAME || "",
         correo: contacto.EMAIL?.[0]?.VALUE || "",
         telefono: contacto.PHONE?.[0]?.VALUE || "",
-        dui: contacto.DUI || "",
+        dui: contacto.DUI?.[0]?.VALUE || "",
         bitrix_id: Number(contacto.ID)
     };
 
